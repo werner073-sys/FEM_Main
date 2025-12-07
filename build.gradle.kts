c983fa98ee41204f5539d2c82577c7e80c836e37
werner073-sys <werner073@gmail.com>
2025-12-07 09:12:52 +0000

    Add GitHub Actions CI/CD workflow for Android testing with emulators
    - Build APK on every push and PR
    - Run instrumented tests on API levels 30, 33, 34
    - Enable KVM for emulator acceleration on GitHub runners
    - Cache AVD snapshots for faster test runs
    - Upload test reports and APK artifacts
    - Run lint checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,11 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
+
 
 allprojects {
     repositories {
